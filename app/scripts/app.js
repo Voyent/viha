f9af0578e4213818d7ed727e03a8aafd53e5b3ac
Brennan-McKinney-Work <brennan.mckinney@icesoft.com>
2016-06-14 16:16:59 -0700

    Another stab at the routing issues.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,16 +25,22 @@
 
     // Sets app default base URL
     app.baseUrl = '/';
+    console.log(window.location.port);
     if (window.location.port === '') {  // if production
       // Uncomment app.baseURL below and
       // set app.baseURL to '/your-pathname/' if running from folder in production
       if(window.location.pathname.indexOf('client.html')!== -1){
-        app.baseUrl = '/demos/viha/client.html/';
+        app.baseUrl = '/demos/viha/client.html';
         //app.baseUrl = '/demos/staging/viha/client.html/';
       }
       else{
         app.baseUrl = '/demos/viha/';
         //app.baseUrl = '/demos/staging/viha/';
+      }
+    }
+    else{
+      if(window.location.pathname.indexOf('client.html') !== -1){
+        app.baseUrl = '/client.html'
       }
     }
 
