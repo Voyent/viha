15721804fe4161eadf1f4079332b15a3da7d4cfb
cgug <carlo.guglielmin@icesoft.com>
2016-07-26 12:04:36 -0600

    Voyent rebranding

--- gulpfile.js
+++ gulpfile.js
@@ -136,7 +136,7 @@ gulp.task('copy', function() {
   // Copy over only the bower_components we need
   // These are things which cannot be vulcanized
   var bower = gulp.src([
-    'app/bower_components/{webcomponentsjs,platinum-sw,sw-toolbox,promise-polyfill,bridgeit-web-components,jQuery-QueryBuilder,components-font-awesome,ace-builds}/**/*'
+    'app/bower_components/{webcomponentsjs,platinum-sw,sw-toolbox,promise-polyfill,voyent-web-components,jQuery-QueryBuilder,components-font-awesome,ace-builds}/**/*'
   ]).pipe(gulp.dest(dist('bower_components')));
 
   return merge(app, bower)
